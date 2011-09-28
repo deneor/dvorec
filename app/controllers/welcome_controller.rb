1cd19467f420cbfb9caf62ff33dedc54191f3edb
Savosin Denis <deneor@yandex.ru>
2011-09-29 00:29:03 +0400

    hm..

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
  before_filter :authenticate_user!, :except => [:index, :show]
  
   def index
+    @user ||= User.new
     @user=current_user
   end
   
